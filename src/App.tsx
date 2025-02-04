f69b72b646d76ad0dd5d3f4c9b78401290c58bcf
Pedro Brasca <pedropaulobrasca@gmail.com>
2025-02-04 15:01:19 -0300

    feat: configura o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { useEnterprisesQuery } from "./services/graphql/queries/enterprises";
 import type { Enterprise } from "./types/enterprise";
+import { Home } from "./pages/home";
 
 const App = () => {
   const { data, loading, error } = useEnterprisesQuery();
@@ -8,21 +10,11 @@ const App = () => {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div>
-      <h1>Enterprises</h1>
-      <ul>
-        {data.enterprises.map((enterprise: Enterprise) => (
-          <li key={enterprise.id}>
-            <h2>{enterprise.name}</h2>
-            <p>{enterprise.commercial_name}</p>
-            <p>{enterprise.cnpj}</p>
-            <p>{enterprise.description}</p>
-            <p>{new Date(enterprise.inserted_at).toLocaleString("pt-BR")}</p>
-            <p>{new Date(enterprise.updated_at).toLocaleString("pt-BR")}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
